02e71caa2a55efd980101ca6fcf3360d27965c6f
Juan Gonzalez Mellizo <juan.david.gonzalez@outlook.com>
2024-10-30 15:38:26 -0600

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,141 @@
+﻿namespace graphsAssignment
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Title title1 = new System.Windows.Forms.DataVisualization.Charting.Title();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.upload = new System.Windows.Forms.Button();
+            this.AddColumn_btn = new System.Windows.Forms.Button();
+            this.columnNameTextBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(2, 31);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 72;
+            this.dataGridView1.RowTemplate.Height = 31;
+            this.dataGridView1.Size = new System.Drawing.Size(712, 280);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // chart1
+            // 
+            chartArea1.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.chart1.Legends.Add(legend1);
+            this.chart1.Location = new System.Drawing.Point(754, 31);
+            this.chart1.Name = "chart1";
+            series1.ChartArea = "ChartArea1";
+            series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Pie;
+            series1.Legend = "Legend1";
+            series1.Name = "Series1";
+            this.chart1.Series.Add(series1);
+            this.chart1.Size = new System.Drawing.Size(605, 652);
+            this.chart1.TabIndex = 1;
+            this.chart1.Text = "chart1";
+            title1.Name = "Title1";
+            title1.Text = "chart1";
+            this.chart1.Titles.Add(title1);
+            // 
+            // upload
+            // 
+            this.upload.Location = new System.Drawing.Point(564, 317);
+            this.upload.Name = "upload";
+            this.upload.Size = new System.Drawing.Size(150, 55);
+            this.upload.TabIndex = 2;
+            this.upload.Text = "upload";
+            this.upload.UseVisualStyleBackColor = true;
+            this.upload.Click += new System.EventHandler(this.upload_Click);
+            // 
+            // AddColumn_btn
+            // 
+            this.AddColumn_btn.Location = new System.Drawing.Point(325, 317);
+            this.AddColumn_btn.Name = "AddColumn_btn";
+            this.AddColumn_btn.Size = new System.Drawing.Size(150, 55);
+            this.AddColumn_btn.TabIndex = 5;
+            this.AddColumn_btn.Text = "Add Column";
+            this.AddColumn_btn.UseVisualStyleBackColor = true;
+            this.AddColumn_btn.Click += new System.EventHandler(this.AddColumn_btn_Click);
+            // 
+            // columnNameTextBox
+            // 
+            this.columnNameTextBox.Location = new System.Drawing.Point(145, 317);
+            this.columnNameTextBox.Name = "columnNameTextBox";
+            this.columnNameTextBox.Size = new System.Drawing.Size(174, 29);
+            this.columnNameTextBox.TabIndex = 6;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(7, 317);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(143, 25);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Column Name:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 24F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1386, 775);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.columnNameTextBox);
+            this.Controls.Add(this.AddColumn_btn);
+            this.Controls.Add(this.upload);
+            this.Controls.Add(this.chart1);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.Button upload;
+        private System.Windows.Forms.Button AddColumn_btn;
+        private System.Windows.Forms.TextBox columnNameTextBox;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace graphsAssignment
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            dataGridView1.ColumnCount = 0;
+
+            AddColumn_btn.Click += AddColumn_btn_Click;
+
+        }
+
+        private void upload_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void AddColumn_btn_Click(object sender, EventArgs e)
+        {
+            string columnName = columnNameTextBox.Text;
+
+            if (!string.IsNullOrEmpty(columnName))
+            {
+                dataGridView1.Columns.Add(columnName, columnName);
+                columnNameTextBox.Clear();
+            }
+        }
+    }
+}
