d8e8c681b1202fb8c28849c6121660318eaa4971
Juan Gonzalez Mellizo <juan.david.gonzalez@outlook.com>
2024-10-31 21:58:01 -0600

    added the upload funtionality to update the pie chart

--- Form1.cs
+++ Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
 
 namespace graphsAssignment
 {
@@ -16,15 +17,35 @@ namespace graphsAssignment
         {
             InitializeComponent();
 
-            dataGridView1.ColumnCount = 0;
+            dataGridView1.ColumnCount = 0;//column count starts at 0
 
-            AddColumn_btn.Click += AddColumn_btn_Click;
+            AddColumn_btn.Click += AddColumn_btn_Click;  //Adds columns into the data grid view when button is pressed
 
+
+            upload.Click += upload_Click;  //uploads the data inside the data grid into the pie chart
         }
 
+        //class that handles the uploading event for the data in the grid view into the pie chart
         private void upload_Click(object sender, EventArgs e)
         {
+            List<string> columnName = new List<string>();
+            List<double> rowValues = new List<double>();
 
+            foreach (DataGridViewRow row in dataGridView1.Rows)
+            {
+                if (row.Cells[0].Value != null && row.Cells[1].Value != null)
+                {
+                    string label = row.Cells[0].Value.ToString();
+                    double value;
+
+                    if (double.TryParse(row.Cells[1].Value.ToString(), out value))
+                    {
+                        columnName.Add(label);
+                        rowValues.Add(value);
+                        UpdatePieChart(columnName, rowValues);
+                    }
+                }
+            }
         }
 
         private void AddColumn_btn_Click(object sender, EventArgs e)
@@ -36,6 +57,28 @@ namespace graphsAssignment
                 dataGridView1.Columns.Add(columnName, columnName);
                 columnNameTextBox.Clear();
             }
+        }
+
+        private void UpdatePieChart(List<string> labels, List<double> values)
+        {
+            chart1.Series.Clear();
+
+            var series = new Series
+            {
+                Name = "PieSeries",
+                ChartType = SeriesChartType.Pie
+            };
+
+            chart1.Series.Add(series);
+
+            for(int i = 0; i < labels.Count; i++)
+            {
+                series.Points.AddXY(labels[i], values[i]);
+            }
+
+            series.IsValueShownAsLabel = true;
+            chart1.Legends[0].Enabled = true;
+
         }
     }
 }
